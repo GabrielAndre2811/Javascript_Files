6ad4e4ea9c1a15a4f14cc6aa1db2a05289e2a6b7
GABRIEL ANDRE GONCALVES <bielandregoncalves@gmail.com>
2025-04-22 17:12:53 -0300

    Add files via upload

---
+++ soma.js
@@ -0,0 +1,8 @@
+function soma(valor1, valor2){
+    return valor1 + valor2;
+}
+
+let valor1 = parseInt(prompt("Digite um número"));
+let valor2 = parseInt(prompt("Digite outro número"));
+let resultado = soma(valor1, valor2);
+console.log("A soma é: " + resultado);
